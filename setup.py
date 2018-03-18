be93b5964725728f0b7bf48a19882701cedc82bb
critical-path <37241479+critical-path@users.noreply.github.com>
2018-03-18 19:55:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     "Programming Language :: Python :: 3"
   ],
 
-  keywords="python lists html translator",
+  keywords="python lists html translator translate",
 
   packages=find_packages(),
 
