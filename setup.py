d2ea1c70e9fc38348d5533f8dc89b0905cd9f0d1
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-28 07:53:50 -0400

    On branch master
     Changes to be committed:
    
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,26 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
 setup(
-  name="py-list-html-translator", 
-  
-  version="1.0.0",
-  
-  description='convert lists to html with py-list-html-translator', 
-
-  url="https://github.com/critical-path/py-list-html-translator",
-
-  author="critical-path",
-
-  author_email="n/a",
-
-  license="MIT",
-
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    
-    "Intended Audience :: Developers",
-
-    "License :: OSI Approved :: MIT License",
-    
-    "Programming Language :: Python :: 3"
-  ],
-
-  keywords="python lists html translator translate",
-
-  packages=find_packages(),
-
-  extras_require={
-    "test": [
-      "py-test"
-    ]
-  }
+    name="py-list-html-translator",
+    version="1.0.0",
+    description="convert lists to html with py-list-html-translator",
+    url="https://github.com/critical-path/py-list-html-translator",
+    author="critical-path",
+    author_email="n/a",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="python lists html translator translate",
+    packages=find_packages(),
+    extras_require={
+        "test": [
+            "pytest"
+        ]
+    }
 )
-
