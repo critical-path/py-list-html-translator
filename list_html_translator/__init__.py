afec3eace42f1dfabb964b511b53104b1ca28b3c
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-28 08:21:26 -0400

    On branch master
     Changes to be committed:
    
    	modified:   list_html_translator/__init__.py

--- list_html_translator/__init__.py
+++ list_html_translator/__init__.py
@@ -1,16 +1,7 @@
 "writing html is tedious.  py-list-html-translator makes it fun!"
 
-"""
-how to use py-list-html-translator:
- 
-from list_html_translator import Translator
-in_list = ["@html"]
-translator = Translator(in_list)
-out_html = translator.translate()
-print(out_html)
-"""
-
 from .list_html_translator import Translator
+
 
 __version__ = "1.0.0"
 
